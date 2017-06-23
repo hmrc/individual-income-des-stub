917e00efdf8acd028905bda38b4a967a3ec0c52f
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2017-06-23 12:40:24 +0100

    OGD-241 - Create employer

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -11,6 +11,7 @@ object MicroServiceBuild extends Build with MicroService {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0",
+    "uk.gov.hmrc" %% "play-auditing" % "2.9.0",
     "uk.gov.hmrc" %% "microservice-bootstrap" % "5.15.0",
     "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
@@ -18,13 +19,16 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "play-hmrc-api" % "1.4.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "domain" % "4.1.0"
+    "uk.gov.hmrc" %% "domain" % "4.1.0",
+    "org.scalacheck" %% "scalacheck" % "1.12.6"
   )
 
   def test(scope: String = "test,it") = Seq(
+    "uk.gov.hmrc" %% "reactivemongo-test" % "2.0.0" % scope,
     "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "org.mockito" % "mockito-core" % "1.10.19" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
 
