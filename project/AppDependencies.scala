5431371024d3c6b3f7f8df608a5596ac6e902db6
johno1985 <1640705+johno1985@users.noreply.github.com>
2023-09-13 12:18:38 +0100

    DLS-6376: Tidied up test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,29 +1,27 @@
-import play.core.PlayVersion
 import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
   val hmrcBootstrapVersion = "7.19.0"
   val mockitoScalaVersion = "1.17.12"
+  val playVersion = "play-28"
 
   val hmrc = "uk.gov.hmrc"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    hmrc                %% "bootstrap-backend-play-28" % "7.14.0",
-    hmrc                %% "domain"                    % "8.1.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "1.1.0",
-    "com.typesafe.play" %% "play-json-joda"            % "2.9.1"
+    hmrc                %% s"bootstrap-backend-$playVersion" % hmrcBootstrapVersion,
+    hmrc                %% "domain"                          % "8.1.0-play-28",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"        % "1.1.0",
+    "com.typesafe.play" %% "play-json-joda"                  % "2.9.1"
   )
 
   def test(scope: String = "test,it"): Seq[ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.2.15"            % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"             % scope,
-    "org.pegdown"            % "pegdown"             % "1.6.0"             % scope,
-    "org.mockito"            % "mockito-scala_2.12"  % "1.17.12"           % scope,
-    "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
-    "org.scalaj"             %% "scalaj-http"        % "2.4.2"             % scope,
-    "com.github.tomakehurst" % "wiremock-jre8"       % "2.27.2"            % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"        % "0.62.2"            % scope
+    hmrc                     %% s"bootstrap-test-$playVersion" % hmrcBootstrapVersion % scope,
+    "org.scalatest"          %% "scalatest"                    % "3.2.15"             % scope,
+    "org.mockito"            %% "mockito-scala"                % mockitoScalaVersion  % scope,
+    "org.scalaj"             %% "scalaj-http"                  % "2.4.2"              % scope,
+    "com.github.tomakehurst" % "wiremock-jre8"                 % "2.27.2"             % scope,
+    "com.vladsch.flexmark"   % "flexmark-all"                  % "0.62.2"             % scope
   )
 }
