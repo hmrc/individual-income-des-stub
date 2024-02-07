ecd4ffbfad57ac8041e106dc3f3493c7fce0f4de
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-02-07 15:11:38 +0000

    DLS-9976: Update play-json-joda to version 2.9.4

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,14 @@ object AppDependencies {
   val hmrcBootstrapVersion = "7.19.0"
   val mockitoScalaVersion = "1.17.12"
   val playVersion = "play-28"
-
   val hmrc = "uk.gov.hmrc"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     hmrc                %% s"bootstrap-backend-$playVersion" % hmrcBootstrapVersion,
     hmrc                %% "domain"                          % "8.1.0-play-28",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"        % "1.1.0",
-    "com.typesafe.play" %% "play-json-joda"                  % "2.9.1"
+    s"$hmrc.mongo"      %% s"hmrc-mongo-$playVersion"        % "1.1.0",
+    "com.typesafe.play" %% "play-json-joda"                  % "2.9.4"
   )
 
   def test(scope: String = "test,it"): Seq[ModuleID] = Seq(
